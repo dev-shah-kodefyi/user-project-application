edaac0c6c4ba6d32ece6dc4d822a95d89c7c2513
devops311 <58652764+devops311@users.noreply.github.com>
2020-02-07 12:50:54 +0530

    Update variables.tf

--- 04-Terraform/03-Ansible-Deploy/variables.tf
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -3,13 +3,13 @@ variable "key_name" {
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/aditikey.pem"
+  default = "/root/.ssh/key.pem"
 }
 
 variable "us-east-zones" {
-  default = ["us-east-2"]
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "sg-id" {
-  default = "sg-04f14b8ba13c81733"
+  default = "sg-0e7a5aca0845681c1"
 }
